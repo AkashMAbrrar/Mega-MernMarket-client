75153a9e1538472567b3ece6a5216014256a1351
AkashAbrrar <mabrrar9@gmail.com>
2022-07-23 23:20:48 +0600

    this commit for top banner with react carousel added

--- grand-bazaar-client/src/components/home/Home.jsx
+++ grand-bazaar-client/src/components/home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import Banner from './Banner';
 import Navbar from './Navbar';
+import { styled } from '@mui/material';
+
+const Component = styled(Box)`
+  padding: 10px;
+  background: #f2f2f2
+`;
 
 const Home = () => {
     return (
-        <Navbar></Navbar>
+        <>
+            <Navbar></Navbar>
+            <Component>
+                <Banner></Banner>
+            </Component>
+        </>
     );
 };
 
