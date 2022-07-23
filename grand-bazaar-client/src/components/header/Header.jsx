bcbd847bf969aa8e25e1b3964ff7bfdc1c4c2fdc
AkashAbrrar <mabrrar9@gmail.com>
2022-07-23 13:59:19 +0600

    plus logo is added

--- grand-bazaar-client/src/components/header/Header.jsx
+++ grand-bazaar-client/src/components/header/Header.jsx
@@ -13,11 +13,17 @@ margin-top: 8px;
 padding: 10px;
 line-height: 0;
 `;
+const PlusLogo = styled('img')({
+    width: 15,
+    heightL: 10,
+    marginBottom: 20,
+    marginLeft: 5
+});
 
 
 const Header = () => {
 
-
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
 
     return (
         <div>
@@ -25,8 +31,10 @@ const Header = () => {
                 <Toolbar>
                     <Component>
                         <img src={logo} alt="Brand-Bazaar" style={{ width: 40, height: 40 }} />
-                        <Box>
-                            <Typography style={{ marginBottom: 15, fontWeight: 700, fontSize: 18, fontStyle: 'italic' }}>Grand Bazaar</Typography>
+                        <Box style={{ display: 'flex' }}>
+                            <Typography style={{ marginBottom: 15, fontWeight: 700, fontSize: 18, fontStyle: 'italic', color: '#FFA07A' }}>Grand Bazaar&nbsp; <Box component="span" style={{ color: '#FFD700' }}>Plus</Box>
+                            </Typography>
+                            <PlusLogo src={subURL} alt="" />
                         </Box>
                     </Component>
                 </Toolbar>
